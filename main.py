6f18f33338184ac8bcef11907dd7441315d1796d
Sky <122771939+Sky1337x@users.noreply.github.com>
2023-10-30 20:47:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from flask import Flask,render_template
+from flask_sqlalchemy import SQLAlchemy
 
+#connecting to the database
+locl_server=True
 app=Flask(__name__,template_folder='templates')
+app.secret_key='test'
+
+app.config['SQLALCHEMY_DATABASE_URI']='mysql://username:@localhost/gymdb'
+db=SQLAlchemy(app)
 
 @app.route('/')
 def Home_page():
     return render_template('index.html')
-
-@app.route('/page1')
-def page_1():
-    return render_template('page1.html')
-
 
 app.run(debug=True)
