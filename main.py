930015353e30cba1ee0cca72a627d69a1eb4430e
Sky <122771939+Sky1337x@users.noreply.github.com>
2023-11-02 14:13:40 +0530

    login and signup are now functional

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
-from flask import Flask,render_template,request,session,redirect
+from flask import Flask,render_template,request,session,redirect,url_for
 from flask_sqlalchemy import SQLAlchemy
 import pymysql
 pymysql.install_as_MySQLdb()
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import login_user,logout_user,LoginManager,login_manager
+from flask_login import login_required, current_user
  
 #connecting to database
 locl_server=True
 app=Flask(__name__, template_folder='templates',static_folder='static')
 
 app.secret_key='test'
+
+login_manager=LoginManager(app)
+login_manager.login_view='login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 app.config['SQLALCHEMY_DATABASE_URI']='mysql://root:@localhost/gymdb'
 db=SQLAlchemy(app)
@@ -25,12 +33,29 @@ class User(UserMixin,db.Model):
 #this is the landing page
 @app.route('/')
 def Home_page():
-
     return render_template('index.html')
 
-@app.route('/login')
+@app.route('/member')
+def member():
+    return render_template('member_area.html')
+
+
+
+@app.route('/login',methods=['POST','GET'])
 def login():
-    return render_template('login.html')
+    if request.method=='POST':
+        email=request.form.get('email')
+        password=request.form.get('password')
+        user=User.query.filter_by(email=email).first()
+
+        if user and check_password_hash(user.password,password):
+            login_user(user)
+            return redirect(url_for('member'))
+        else:
+            print("invalid credentials")
+            return render_template("mem_login.html")
+    
+    return render_template('mem_login.html')
 
 @app.route('/signup',methods=['post','get'])
 def signup():
@@ -42,7 +67,7 @@ def signup():
         user=User.query.filter_by(email=email).first()
         if user:
             print("email already exists")
-            return render_template('/signup.html')
+            return render_template('mem_signup.html')
         
         encpassword=generate_password_hash(password)
 
@@ -50,10 +75,13 @@ def signup():
         newuser=User(username=username,email=email,password=encpassword)
         db.session.add(newuser)
         db.session.commit()
-        return render_template('login.html')
+        return render_template('mem_login.html')
+        
     
 
-    return render_template('signup.html')
+    return render_template('mem_signup.html')
+
+
 
 
   
